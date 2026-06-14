e3b05d3a1e0000d28eb5472219f08b627a27c627
srtopuzova <siyana.r.topuzova.2023@elsys-bg.org>
2026-06-14 19:44:08 +0300

    Fixed formatting issues and added coordinate validation

--- game.c
+++ game.c
@@ -5,56 +5,66 @@
 void initPlayer(Player *p, const char *name)
 {
     strcpy(p->name, name);
-        p->shipsLeft = SHIPS;
+    p->shipsLeft = SHIPS;
 
-            for(int i=0;i<SIZE;i++)
-                    for(int j=0;j<SIZE;j++)
-                                p->board[i][j]='.';
-                                }
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            p->board[i][j] = '.';
+        }
+    }
+}
 
-                                void placeShips(Player *p)
-                                {
-                                    int r,c;
+void placeShips(Player *p)
+{
+    int r, c;
 
-                                        printf("\n%s place %d ships\n", p->name, SHIPS);
+    printf("\n%s place %d ships\n", p->name, SHIPS);
 
-                                            for(int i=0;i<SHIPS;i++)
-                                                {
-                                                        printf("Ship %d (row col): ",i+1);
-                                                                scanf("%d %d",&r,&c);
+    for(int i = 0; i < SHIPS; i++)
+    {
+        printf("Ship %d (row col): ", i + 1);
+        scanf("%d %d", &r, &c);
 
-                                                                        if(r<0 || r>=SIZE || c<0 || c>=SIZE || p->board[r][c]=='S')
-                                                                                {
-                                                                                            i--;
-                                                                                                        continue;
-                                                                                                                }
+        if(r < 0 || r >= SIZE || c < 0 || c >= SIZE || p->board[r][c] == 'S')
+        {
+            i--;
+            continue;
+        }
 
-                                                                                                                        p->board[r][c]='S';
-                                                                                                                            }
-                                                                                                                            }
+        p->board[r][c] = 'S';
+    }
+}
 
-                                                                                                                            void printBoard(Player *p)
-                                                                                                                            {
-                                                                                                                                for(int i=0;i<SIZE;i++)
-                                                                                                                                    {
-                                                                                                                                            for(int j=0;j<SIZE;j++)
-                                                                                                                                                        printf("%c ",p->board[i][j]);
+void printBoard(Player *p)
+{
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            printf("%c ", p->board[i][j]);
+        }
 
-                                                                                                                                                                printf("\n");
-                                                                                                                                                                    }
-                                                                                                                                                                    }
+        printf("\n");
+    }
+}
 
-                                                                                                                                                                    int attack(Player *attacker, Player *defender, int row, int col)
-                                                                                                                                                                    {
-                                                                                                                                                                        (void)attacker;
+int attack(Player *attacker, Player *defender, int row, int col)
+{
+    (void)attacker;
 
-                                                                                                                                                                            if(defender->board[row][col]=='S')
-                                                                                                                                                                                {
-                                                                                                                                                                                        defender->board[row][col]='X';
-                                                                                                                                                                                                defender->shipsLeft--;
-                                                                                                                                                                                                        return 1;
-                                                                                                                                                                                                            }
+    if(row < 0 || row >= SIZE || col < 0 || col >= SIZE)
+    {
+        return 0;
+    }
 
-                                                                                                                                                                                                                return 0;
-                                                                                                                                                                                                                }
-                                                                                                                                                                                                                
+    if(defender->board[row][col] == 'S')
+    {
+        defender->board[row][col] = 'X';
+        defender->shipsLeft--;
+        return 1;
+    }
+
+    return 0;
+}
